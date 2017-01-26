92a42db556c05e07f17a96ccbb6dc0b308214118
Jim Ladino <jlstar1.jl@gmail.com>
2017-01-26 04:10:45 +0000

    Add Phases document to project

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Foods.create!([
+  {food_name: "Jimmy Toast", cost: "50.00", unit_qty: "Per Slice"}
+])
+Meals.create!([
+  {image: "pancakes.jpg", meal_name: "Pancakes", category: "Breakfast", prep_time: "20 Minutes", servings: "8"},
+  {image: "", meal_name: "Spaghetti & Meatballs", category: "Supper / Dinner", prep_time: "20 Minutes", servings: "8"},
+  {image: "eggs.jpg", meal_name: "Eggs", category: "Breakfast", prep_time: "5 Minutes", servings: "1"}
+])
