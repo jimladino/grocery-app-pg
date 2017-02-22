95257dbb506d02fca0af93c141143fadf804c9b2
Jim Ladino <jlstar1.jl@gmail.com>
2017-02-22 04:15:11 +0000

    Add id colums to tables.  Modify Project Phases.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,13 @@ Foods.create!([
 ])
 Meals.create!([
   {image: "pancakes.jpg", meal_name: "Pancakes", category: "Breakfast", prep_time: "20 Minutes", servings: "8"},
-  {image: "", meal_name: "Spaghetti & Meatballs", category: "Supper / Dinner", prep_time: "20 Minutes", servings: "8"},
-  {image: "eggs.jpg", meal_name: "Eggs", category: "Breakfast", prep_time: "5 Minutes", servings: "1"}
+  {image: "pizza.jpg", meal_name: "Pizza", category: "Dinner", prep_time: "30 Minutes", servings: "8"},
+  {image: "eggs.jpg", meal_name: "Eggs & Toast", category: "Breakfast", prep_time: "5 Minutes", servings: "1"},
+  {image: "https://www.sbarro.com/wp-content/uploads/2015/04/12-sbarro-spaghetti-meatballs.jpg", meal_name: "Spaghetti & Meatballs", category: "Dinner", prep_time: "15 Minutes", servings: "8"},
+  {image: "brunch.jpg", meal_name: "Eggs, Sausage, Hash Browns & Waffles", category: "Brunch", prep_time: "40 Minues", servings: "8"},
+  {image: "pbj.jpg", meal_name: "Peanut Butter & Jelly Sandwich", category: "Lunch", prep_time: "2 Minutes", servings: "1"},
+  {image: "https://www.deltaco.com/images/specials/product-left-slide1.png", meal_name: "Tacos", category: "Dinner", prep_time: "30 Minutes", servings: "8"},
+  {image: "https://silk.com/sites/default/files/recipes/medium/Oatmeal_shutterstock_173846588_RT.gif", meal_name: "Oatmeal", category: "Breakfast", prep_time: "10 Minutes", servings: "8"},
+  {image: "http://www.fridaycakenight.com/wp-content/uploads/2015/12/Waffles1-745x1024.jpg", meal_name: "Waffles & Fruit", category: "Breakfast", prep_time: "15 Minutes", servings: "8"},
+  {image: "https://theprimepursuit.files.wordpress.com/2012/08/dsc06446.jpg", meal_name: "Cold Cereal", category: "Breakfast", prep_time: "2 Minutes", servings: "1"}
 ])
