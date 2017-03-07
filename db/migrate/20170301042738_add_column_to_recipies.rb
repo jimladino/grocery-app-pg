8c67d75f309988cab51685dfb65a836693de65cf
Jim Ladino <jlstar1.jl@gmail.com>
2017-03-07 04:16:41 +0000

    correct migrations

--- db/migrate/20170301042738_add_column_to_recipies.rb
+++ db/migrate/20170301042738_add_column_to_recipies.rb
@@ -3,6 +3,5 @@ class AddColumnToRecipies < ActiveRecord::Migration
     add_column :recipies, :meal_id, :integer
     add_column :recipies, :food_id, :integer
     add_column :recipies, :instructions, :string
-    add_column :recipies, :meal_name, :string
   end
 end
